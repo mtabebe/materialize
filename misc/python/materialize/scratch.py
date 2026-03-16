66b112f789bc46effb33408cf634333a6eff2a23
mtabebe <michael.abebe@materialize.com>
2026-03-16 13:31:57 -0400

    fix up scratch.py

--- misc/python/materialize/scratch.py
+++ misc/python/materialize/scratch.py
@@ -499,15 +499,15 @@ def launch_cluster(
         for d in descs
     ]
 
-    loop = asyncio.get_event_loop()
-    loop.run_until_complete(
-        asyncio.gather(
+    async def _setup_all():
+        await asyncio.gather(
             *(
                 setup(i, git_rev if d.checkout else "HEAD")
                 for (i, d) in zip(instances, descs)
             )
         )
-    )
+
+    asyncio.run(_setup_all())
 
     for i in instances:
         i.reload()
